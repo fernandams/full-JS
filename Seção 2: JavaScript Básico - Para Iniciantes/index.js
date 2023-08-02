22c2cb36bacec47980571bb29d9d8482cadb43dc
fernandams <nanda.sousa.m@gmail.com>
2023-08-02 12:16:43 -0300

    :sparkles: Adds first class about Javascript: console.log()
    Content of first JavaScript module: Basics to beginners

---
+++ Seção 2: JavaScript Básico - Para Iniciantes/index.js
@@ -0,0 +1,5 @@
+console.log('Fernanda "Macedo"'); // String = Texto
+console.log("Fernanda 'Macedo'");
+console.log(`"Fernanda" 'Macedo'`);
+
+console.log(35, 15.85, 'Luiz Otávio'); // 35 e 15.85 = Number = Número

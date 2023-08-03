8d3790d50e5cdf084dd3a7c26c355b24faf0a568
fernandams <nanda.sousa.m@gmail.com>
2023-08-03 09:34:23 -0300

    :sparkles: Adds JS into HTML, part 2
    Content of first JavaScript module: Basics to beginners

---
+++ Section 2: Basic JavaScript - For Beginners/3 - HTML + JavaScript/js/index.js
@@ -0,0 +1,5 @@
+// Um comentário
+console.log('Olá mundo!');
+console.log('Esse trecho será exibido no console do navegador.');
+alert('Olá mundo!'); // esse alerta é exibido no navegador mas no node não é definido 
+// (o node e o navegador são ambientes diferentes)

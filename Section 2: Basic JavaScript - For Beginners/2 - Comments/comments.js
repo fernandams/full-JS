15037d860f4cb8bf7efae08a0db17a94ee76eee2
fernandams <nanda.sousa.m@gmail.com>
2023-08-03 09:16:44 -0300

    :sparkles: Adds JS comments
    Content of first JavaScript module: Basics to beginners

---
+++ Section 2: Basic JavaScript - For Beginners/2 - Comments/comments.js
@@ -0,0 +1,19 @@
+// Escrever um comentário aqui
+console.log('Olá mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+console.log('Linha 2');
+/**
+ * Título do comentário
+ * 
+ * Um bloco de comentario
+ * Continuação do comentário na outra linha
+ */
+console.log('Linha 3');
+// comentário
+
+console.log('Linha 4');
+/*
+console.log('Linha 1');
+*/
+
+// vai ser ignorado

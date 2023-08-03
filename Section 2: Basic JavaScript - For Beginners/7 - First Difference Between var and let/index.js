479ad609a80df11f3404bcbb5bce20bb08c94948
fernandams <nanda.sousa.m@gmail.com>
2023-08-03 11:44:15 -0300

    :sparkles: Introduces one difference between let and var
    Content of first JavaScript module: Basics to beginners

---
+++ Section 2: Basic JavaScript - For Beginners/7 - First Difference Between var and let/index.js
@@ -0,0 +1,12 @@
+// ECMAScript 2015 (ES6) introduziu o let
+// o var permite a redeclaração de variável, enquanto o let não permite
+
+nome = 'José'; // é possível criar variáveis globais assim mas NÃO FAÇA ISSO!
+console.log(nome);
+
+var nome = 'Luiz';
+console.log(nome);
+
+var nome = 'Otávio';
+console.log(nome);
+

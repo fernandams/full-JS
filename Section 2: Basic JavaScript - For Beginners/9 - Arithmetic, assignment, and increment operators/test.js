aa614e2c1e0f5d415fdeaf98c0f8db300c5f2238
fernandams <nanda.sousa.m@gmail.com>
2023-08-08 15:35:20 -0300

    :sparkles: Adds my particular test cases
    Content of first JavaScript module: Basics to beginners

---
+++ Section 2: Basic JavaScript - For Beginners/9 - Arithmetic, assignment, and increment operators/test.js
@@ -0,0 +1,18 @@
+let test = '1.5';
+console.log(test, typeof test);
+
+test = Number(test); 
+console.log(test, typeof test);
+
+test = '2.5';
+console.log(test, typeof test);
+
+test = parseFloat(test);
+console.log(test, typeof test);
+
+test = '3.5';
+console.log(test, typeof test);
+
+test = parseInt(test);
+console.log(test, typeof test);
+

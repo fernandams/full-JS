d8eb28a5f9e9cc22e4797a2335e32d1d3d5c8b79
fernandams <nanda.sousa.m@gmail.com>
2023-08-09 16:10:17 -0300

    :sparkles: Adds my solution to the proposed exercise
    Content of first JavaScript module: Basics to beginners

---
+++ Section 2: Basic JavaScript - For Beginners/10 - Alert, confirm and prompt (Browser)/js/exercise.js
@@ -0,0 +1,6 @@
+let num1 = window.prompt('Digite o primeiro número:');
+let num2 = window.prompt('Digite o segundo número:');
+
+let resultado = Number(num1) + Number(num2);
+
+alert(`O resultado da soma é: ${resultado}`);

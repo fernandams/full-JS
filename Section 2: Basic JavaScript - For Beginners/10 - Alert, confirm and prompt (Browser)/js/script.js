1ff6fb9daefec836d6c6c3b5986895ffb12dfe48
fernandams <nanda.sousa.m@gmail.com>
2023-08-09 16:01:44 -0300

    :sparkles: Adds alert, prompt and confirm window methods
    Content of first JavaScript module: Basics to beginners

---
+++ Section 2: Basic JavaScript - For Beginners/10 - Alert, confirm and prompt (Browser)/js/script.js
@@ -0,0 +1,16 @@
+// Funções/Métodos dentro do objeto window:
+alert('Com a nossa mensagem.');
+window.confirm('Deseja realmente apagar?');
+window.prompt('Digite o seu nome:');
+let confirma = confirm('Qualquer mensagem');
+
+let num1 = prompt('Digite um numero');
+let num2 = prompt('Digite outro numero');
+console.log(num1+num2); 
+console.log(typeof num1); 
+console.log(typeof num2);
+num1 = parseFloat(num1);
+console.log(typeof num1); 
+num2 = Number(num2);
+console.log(typeof num2);
+console.log(num1+num2); 

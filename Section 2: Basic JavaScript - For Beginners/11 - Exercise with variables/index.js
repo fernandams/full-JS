9d3141518c8f2bea18434275da40183d2df9c386
fernandams <nanda.sousa.m@gmail.com>
2023-08-10 09:14:49 -0300

    :zap: Improves the solution to the variables exercise
    Content of first JavaScript module: Basics to beginners

--- Section 2: Basic JavaScript - For Beginners/11 - Exercise with variables/index.js
+++ Section 2: Basic JavaScript - For Beginners/11 - Exercise with variables/index.js
@@ -2,9 +2,6 @@ let varA = 'A'; // B
 let varB = 'B'; // C
 let varC = 'C'; // A
 
-const temp = varA;
-varA = varB;
-varB = varC;
-varC = temp;
+[varA, varB, varC] = [varB, varC, varA];
 
-console.log(varA,varB,varC);
+console.log(varA, varB, varC);

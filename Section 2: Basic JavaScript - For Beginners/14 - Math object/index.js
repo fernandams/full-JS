a5f9a7062acce61f2c28fcf3262f71c9f7bb74ef
fernandams <nanda.sousa.m@gmail.com>
2023-08-22 15:01:42 -0300

    :sparkles: Adds examples about JS Math object
    Content of first JavaScript module: Basics to beginners

---
+++ Section 2: Basic JavaScript - For Beginners/14 - Math object/index.js
@@ -0,0 +1,22 @@
+let num1 = 9.49;
+let num2 = Math.floor(num1);
+console.log(num2);
+num2 = Math.ceil(num1);
+console.log(num2);
+num2 = Math.round(num1);
+console.log(num2);
+console.log(Math.max(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+console.log(Math.min(1,2,3,4,5,-10,-50,1500,9,8,7,6));
+const aleatorio = Math.round(Math.random() * (10 - 5) + 5);
+console.log(aleatorio);
+const pi = Math.PI;
+console.log(pi);
+
+console.log(Math.pow(2, 10));
+console.log(2 ** 10);
+
+num1 = 25;
+let raizDeNum1 = num1 ** (1/2);
+console.log(raizDeNum1);
+
+console.log(100 / 0);

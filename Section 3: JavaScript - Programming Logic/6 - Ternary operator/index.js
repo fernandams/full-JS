921f5dd97f92559e36bee66a25dd702c84ea0fba
fernandams <nanda.sousa.m@gmail.com>
2023-10-16 16:55:59 -0300

    :sparkles: Adds example with ternary operator
    Content of second JavaScript module: Programming Logic

---
+++ Section 3: JavaScript - Programming Logic/6 - Ternary operator/index.js
@@ -0,0 +1,13 @@
+/**
+ * (condicao) ? 'Valor para verdadeiro' : 'Valor para falso';
+*/
+const pontuacaoUsuario =1000;
+
+// if (pontuacaoUsuario >= 1000) {
+//     console.log('Usuário VIP');
+// } else {
+//     console.log('Usuário normal');
+// }
+
+const nivelUsuario = pontuacaoUsuario >= 1000 ? 'Usuário VIP' : 'Usuário normal';
+console.log(nivelUsuario);

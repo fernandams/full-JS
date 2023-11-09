deb134336b3b7e00836398c82806c8a2075b28ce
fernandams <nanda.sousa.m@gmail.com>
2023-11-09 13:20:52 -0300

    :sparkles: Third solution to the proposed exercise
    Third solution for the proposed exercise, similar to the first solution but replacing the switch with an array. Contents of the second JavaScript module: Programming Logic.

--- Section 3: JavaScript - Programming Logic/9 - Exercise with Switch and Date/assets/js/main.js
+++ Section 3: JavaScript - Programming Logic/9 - Exercise with Switch and Date/assets/js/main.js
@@ -105,13 +105,50 @@
  * Segunda solução:
  */
 
+// const h1 = document.querySelector('.container h1');
+// const data = new Date();
+// const opcoes = {
+//     dateStyle: 'full',
+//     timeStyle: 'short'
+// };
+
+// // h1.innerHTML = data.toLocaleDateString('pt-BR', opcoes);
+
+// h1.innerHTML = new Intl.DateTimeFormat('pt-BR', opcoes).format(data);
+
+/**
+ * Terceira solução:
+ */
+
 const h1 = document.querySelector('.container h1');
 const data = new Date();
-const opcoes = {
-    dateStyle: 'full',
-    timeStyle: 'short'
-};
 
-// h1.innerHTML = data.toLocaleDateString('pt-BR', opcoes);
+function getDiaSemanaTexto(diaSemana) {
+    const diasSemana = ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'];
+    return diasSemana[diaSemana];
+}
 
-h1.innerHTML = new Intl.DateTimeFormat('pt-BR', opcoes).format(data);
+function getNomeMes(numeroMes) {
+    const meses = ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho',
+        'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'];
+    return meses[numeroMes];
+}
+
+function zeroAEsquerda(num) {
+    return num >= 10 ? num : `0${num}`;
+}
+
+function criaData(data) {
+    const diaSemana = data.getDay();
+    const numeroMes = data.getMonth();
+
+    const nomeDia = getDiaSemanaTexto(diaSemana);
+    const nomeMes = getNomeMes(numeroMes);
+
+    return (`${nomeDia}, ${data.getDate()} de ${nomeMes}` +
+        ` de ${data.getFullYear()} às ` +
+        `${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`
+    );
+}
+
+h1.innerHTML = criaData(data);

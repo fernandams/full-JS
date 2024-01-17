2dc6ba9ab97bd28c16a6dc137b630b0664120b54
fernandams <nanda.sousa.m@gmail.com>
2024-01-17 17:38:37 -0300

    :sparkles: Add solution to exercise using for
    Contents of the second JavaScript module: Programming Logic.

---
+++ Section 3: JavaScript - Programming Logic/14 - Exercises using For/assets/js/main.js
@@ -0,0 +1,18 @@
+const elementos = [
+    { tag: 'p', texto: 'Frase 1' },
+    { tag: 'div', texto: 'Frase 2' },
+    { tag: 'section', texto: 'Frase 3' },
+    { tag: 'footer', texto: 'Frase 4' },
+];
+
+const container = document.querySelector('.container');
+const div = document.createElement('div');
+
+for (let i = 0; i < elementos.length; i++) {
+    let { tag, texto } = elementos[i];
+    let tagCriada = document.createElement(tag);
+    tagCriada.innerText = texto;
+    div.appendChild(tagCriada);
+}
+
+container.appendChild(div);

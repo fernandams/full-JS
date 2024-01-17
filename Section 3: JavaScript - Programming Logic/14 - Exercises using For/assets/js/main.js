b30ea2f1c637bf5606216aae3bed1879faad839a
fernandams <nanda.sousa.m@gmail.com>
2024-01-17 17:42:24 -0300

    :art: Add better solution for exercise using for
    Contents of the second JavaScript module: Programming Logic.

--- Section 3: JavaScript - Programming Logic/14 - Exercises using For/assets/js/main.js
+++ Section 3: JavaScript - Programming Logic/14 - Exercises using For/assets/js/main.js
@@ -11,7 +11,9 @@ const div = document.createElement('div');
 for (let i = 0; i < elementos.length; i++) {
     let { tag, texto } = elementos[i];
     let tagCriada = document.createElement(tag);
-    tagCriada.innerText = texto;
+    let textoCriado = document.createTextNode(texto);
+
+    tagCriada.appendChild(textoCriado);
     div.appendChild(tagCriada);
 }
 

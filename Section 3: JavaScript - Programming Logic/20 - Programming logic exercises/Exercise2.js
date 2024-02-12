b32b02aec733955731d533d053ba19a63be38b31
Fernanda Macedo <nanda.sousa.m@gmail.com>
2024-02-12 19:59:55 -0300

    ✨ Add second exercise
    Contents of the second JavaScript module: Programming Logic.

---
+++ Section 3: JavaScript - Programming Logic/20 - Programming logic exercises/Exercise2.js
@@ -0,0 +1,10 @@
+/**
+ * Escreva uma função chamada ePaisagem que recebe dois argumentos, largura e altura de uma imagem (number).
+ * Retorne true se a imagem estiver no modo paisagem.
+ */
+// function ePaisagem(largura, altura) {
+//     return largura > altura;
+// }
+
+const ePaisagem = (largura, altura) => largura > altura;
+console.log(ePaisagem(1920, 1080));
